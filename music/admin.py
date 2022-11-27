524b767768d046d554730f28d3a1ab992e9fa4f4
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-11-27 23:36:23 +0900

    add

--- music/admin.py
+++ music/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import instrument, language, teaching_inst, teaching_language
+from .models import instrument, language, teaching_inst, tl, fee, academic, experience, certificate, reputation, message, oneword
 
 admin.site.register(instrument)
 admin.site.register(language)
 admin.site.register(teaching_inst)
-admin.site.register(teaching_language)
+admin.site.register(tl)
+admin.site.register(fee)
+admin.site.register(academic)
+admin.site.register(experience)
+admin.site.register(certificate)
+admin.site.register(reputation)
+admin.site.register(message)
+admin.site.register(oneword)
+
+#admin.site.register()
 
 # Register your models here.
--- music/models.py
+++ music/models.py
@@ -10,7 +10,31 @@ class language(models.Model):
 class teaching_inst(models.Model):
     teaching_inst = models.ForeignKey(instrument, on_delete=models.CASCADE) 
 
-class teathing_language(models.Model):    
-    teaching_inst = models.ForeignKey(language, on_delete=models.CASCADE) 
+class tl(models.Model):    
+    teaching_lang = models.ForeignKey(language, on_delete=models.CASCADE) 
+
+ #class movie(models.Model):
+  #  prof_movie = models.movieField(movie, on_delete=models.CASCADE) 
+  # 
+class fee(models.Model):
+    fee = models.IntegerField(max_length=3)
+
+class academic(models.Model):
+    academic = models.TextField()
+
+class experience(models.Model):
+    experience = models.TextField()
+
+class certificate(models.Model):
+    certificate = models.TextField()                  
+
+class reputation(models.Model):
+    reoutation = models.TextField()
+
+class message(models.Model):
+    message = models.TextField()
+
+class oneword(models.Model):
+    oneword = models.TextField()       
 
 # Create your models here.
