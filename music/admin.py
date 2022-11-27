0f44216728cdf9fd7b37ce9ba077c64a0123860d
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-11-27 20:22:08 +0900

    add

--- music/admin.py
+++ music/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import instrument, language
+from .models import instrument, language, teaching_inst, teaching_language
 
 admin.site.register(instrument)
 admin.site.register(language)
-
+admin.site.register(teaching_inst)
+admin.site.register(teaching_language)
 
 # Register your models here.
--- music/models.py
+++ music/models.py
@@ -5,5 +5,12 @@ class instrument(models.Model):
     inst_image = models.ImageField(blank=True, null=True)
 
 class language(models.Model):
-    language_name =  models.CharField(max_length=20)   
+    language_name =  models.CharField(max_length=20)
+
+class teaching_inst(models.Model):
+    teaching_inst = models.ForeignKey(instrument, on_delete=models.CASCADE) 
+
+class teathing_language(models.Model):    
+    teaching_inst = models.ForeignKey(language, on_delete=models.CASCADE) 
+
 # Create your models here.
