19279d8fd115d20e529550d3c71d0c62df9e9ff7
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-12-05 19:28:31 +0900

    add

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+#from django.urls import path
 
 #from .migrations import views
 from . import views
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
 
@@ -10,3 +18,4 @@ urlpatterns = [
 
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
