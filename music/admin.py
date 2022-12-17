fbbad2392c10627fe32261c492d9fab2e3672693
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-12-17 10:33:28 +0900

    add

--- music/admin.py
+++ music/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import instrument, teaching_inst, teaching_langage, Teacher, language, teacher_picture
+from .models import instrument,Teacher, language, teacher_picture
 admin.site.register(instrument)
 admin.site.register(language)
-admin.site.register(teaching_inst)
-admin.site.register(teaching_langage)
 admin.site.register(Teacher)
 admin.site.register(teacher_picture)
 
