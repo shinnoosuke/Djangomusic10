0884708ae945218a89b96b5d964e7de1f2ce8658
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-12-22 13:44:43 +0900

    add

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,24 @@
+#from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.models import User
+
+
+#class SignupForm(UserCreationForm):
+#     class Meta:
+#          model = User
+#          fields = ('username',)
+
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from .models import User
+from django import forms
 
 
 class SignupForm(UserCreationForm):
+     email = forms.EmailField(
+        max_length=254,
+        help_text='必須 有効なメールアドレスを入力してください。',
+        label='Eメールアドレス'
+    )
      class Meta:
           model = User
-          fields = ('username',)
+          fields = ('email', 'password1', 'password2', )
