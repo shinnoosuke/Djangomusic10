3c97fab3c46ac6f9ac9df1449720be378eb0389c
shinnoosuke <shinya.iwamoto1022@icloud.com>
2022-12-25 22:14:42 +0900

    add

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,4 +21,4 @@ class SignupForm(UserCreationForm):
     )
      class Meta:
           model = User
-          fields = ('email', 'password1', 'password2', )
+          fields = ('email', 'password1', 'password2','first_name','last_name','city','introduction','image','is_musician')
